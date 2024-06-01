29047c4c9a626be4807e2a0ccbc670fdc3e7dc60
Gabriel Azevedo <gabrielp.azevedo@outlook.com>
2024-06-01 02:11:22 -0300

    :sparkles: feat: add banner to home page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import Slider from './components/Slider';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slider />
+      <Banner />
     </div>
   );
 }
---
+++ Client/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+
+    return (
+        <div className="image-container">
+            <img src="http://127.0.0.1:8080/banner.png" alt="Banner Promocional" className="image"></img>
+        </div>
+    );
+}
